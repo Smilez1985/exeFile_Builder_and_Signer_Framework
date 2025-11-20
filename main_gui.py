ef53f2f84366eb1358efdf5afe71d13bd0de64c9
Smilez1985 <smilez@gmx.net>
2025-11-20 19:37:13 +0100

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -1,13 +1,26 @@
 import sys
-from pathlib import Path
 import tkinter as tk
+from pathlib import Path
+from src.utils.helpers import log
 
 # Pfad-Fix
 sys.path.append(str(Path(__file__).parent))
 
 from src.ui.gui import AppGUI
 
+# Versuche Drag & Drop Support zu laden
+try:
+    from tkinterdnd2 import TkinterDnD
+    DND_AVAILABLE = True
+except ImportError:
+    DND_AVAILABLE = False
+    log.warning("Drag & Drop Lib (tkinterdnd2) nicht gefunden. Feature deaktiviert.")
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = AppGUI(root)
+    if DND_AVAILABLE:
+        root = TkinterDnD.Tk()
+    else:
+        root = tk.Tk()
+        
+    app = AppGUI(root, dnd_enabled=DND_AVAILABLE)
     root.mainloop()
